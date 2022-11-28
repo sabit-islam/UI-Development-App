e86162221071224f754dd30be54eac21ae417535
Sabit Islam <islamsabit@gmail.com>
2022-11-28 11:26:09 -0500

    Initial Commit

---
+++ driver app/driver_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  driver_appApp.swift
+//  driver app
+//
+//  Created by Sabit Islam on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct driver_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
